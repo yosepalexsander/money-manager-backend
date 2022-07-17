b63d4931aa8befcbdd44e8dcab2bec2827682807
yosep <yosepalexsander9@gmail.com>
2022-07-17 11:25:56 +0700

    fix: adjustment

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-// import dotenv and call config function to load environment
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
 
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-// Get routes to the variabel
-const router = require('./src/routes');
+const router = require("./src/routes");
+
+const port = process.env.PORT;
 
 const app = express();
 
-const port = 5000;
-
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-// Add endpoint grouping and router
-app.use('/api/v1/', router);
-app.use('/uploads', express.static('uploads'));
+app.use("/api/v1/", router);
+app.use("/uploads", express.static("uploads"));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
